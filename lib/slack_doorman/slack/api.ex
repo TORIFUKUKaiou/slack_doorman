e047170faf08c2462c6beef19919b7da238c4f6b
TORIFUKU Kaiou <torifuku.kaiou@gmail.com>
2023-08-15 11:01:34 +0900

    https://api.slack.com/methods/users.list

--- lib/slack_doorman/slack/api.ex
+++ lib/slack_doorman/slack/api.ex
@@ -24,8 +24,25 @@ defmodule SlackDoorman.Slack.Api do
     response
   end
 
+  def users_list(cursor \\ nil) do
+    # https://api.slack.com/methods/users.list
+    url = "https://slack.com/api/users.list?limit=1000&cursor=#{cursor}"
+
+    headers = [
+      "Content-type": "application/json",
+      Authorization: "Bearer #{slack_bot_token()}"
+    ]
+
+    {:ok, response} = HTTPoison.get(url, headers)
+    response
+  end
+
   def all_conversations_list() do
     do_all_conversations_list(nil, [])
+  end
+
+  def all_users_list() do
+    do_all_users_list(nil, [])
   end
 
   defp do_all_conversations_list("", channels) do
@@ -49,6 +66,29 @@ defmodule SlackDoorman.Slack.Api do
       |> Kernel.++(got_channels)
 
     do_all_conversations_list(new_next_cursor, new_got_channels)
+  end
+
+  defp do_all_users_list("", users) do
+    users
+  end
+
+  defp do_all_users_list(next_cursor, got_users) do
+    {:ok,
+     %{
+       "ok" => true,
+       "members" => members,
+       "response_metadata" => %{"next_cursor" => new_next_cursor}
+     }} =
+      users_list(next_cursor)
+      |> Map.get(:body)
+      |> Jason.decode()
+
+    new_got_users =
+      members
+      |> Enum.reject(& &1["is_bot"])
+      |> Kernel.++(got_users)
+
+    do_all_users_list(new_next_cursor, new_got_users)
   end
 
   defp slack_bot_token do
