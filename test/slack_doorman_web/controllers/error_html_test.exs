c8e79afa128d8fe21f2d268b6e564521645b46e3
TORIFUKU Kaiou <torifuku.kaiou@gmail.com>
2023-08-15 09:58:24 +0900

    mix format

--- test/slack_doorman_web/controllers/error_html_test.exs
+++ test/slack_doorman_web/controllers/error_html_test.exs
@@ -9,6 +9,7 @@ defmodule SlackDoormanWeb.ErrorHTMLTest do
   end
 
   test "renders 500.html" do
-    assert render_to_string(SlackDoormanWeb.ErrorHTML, "500", "html", []) == "Internal Server Error"
+    assert render_to_string(SlackDoormanWeb.ErrorHTML, "500", "html", []) ==
+             "Internal Server Error"
   end
 end
